917010db9fb5b980a537e3cf0031fda32fd9a93d
DushyanthvarmaK <143kdv@gmail.com>
2024-07-04 13:19:30 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TaskInput from './components/TaskInput/TaskInput';
+import TaskList from './components/TaskList/TaskList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: JSON.parse(localStorage.getItem('tasks')) || [],
+    editTask: null
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.tasks !== this.state.tasks) {
+      localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+    }
+  }
+
+  addTask = (task) => {
+    this.setState((prevState) => ({
+      tasks: [...prevState.tasks, { id: Date.now(), text: task, completed: false }]
+    }));
+  };
+
+  deleteTask = (id) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.filter((task) => task.id !== id)
+    }));
+  };
+
+  updateTask = (id, newText) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.map((task) =>
+        task.id === id ? { ...task, text: newText } : task
+      ),
+      editTask: null
+    }));
+  };
+
+  editCurrentTask = (task) => {
+    this.setState({ editTask: task });
+  };
+
+  toggleComplete = (id) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    }));
+  };
+
+  render() {
+    const { tasks, editTask } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Todo App</h1>
+        <TaskInput
+          addTask={this.addTask}
+          editTask={editTask}
+          updateTask={this.updateTask}
+        />
+        <TaskList
+          tasks={tasks}
+          deleteTask={this.deleteTask}
+          editCurrentTask={this.editCurrentTask}
+          toggleComplete={this.toggleComplete}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
